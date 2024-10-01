871a6e375d6704cf6f4c3dc3719b75ffe6f8dcbd
NEWJIN <winniepooh00@naver.com>
2024-10-01 16:00:55 +0900

    feat: nav 페이지 연결

--- src/components/common/Nav.tsx
+++ src/components/common/Nav.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link';
 
 function Nav() {
   return (
@@ -6,19 +7,25 @@ function Nav() {
       <div className="relative w-full h-[57px]">
         <div className="absolute w-28 m-auto left-0 top-[13px]">
           <div className="absolute w-28 h-[31px] bg-[#d9d9d9]" />
-          <div className="absolute left-[20px] top-[3px] text-black text-xl font-bold font-pretendard">가로 로고</div>
         </div>
 
-        <div className="absolute right-80 top-[16px] text-[#c7c7c7] text-xl font-bold font-pretendard">질문게시판</div>
+        {/* 질문게시판 링크 */}
+        <Link href="/qna" className="absolute right-80 top-[16px] text-[#c7c7c7] text-xl font-bold font-pretendard">
+          질문게시판
+        </Link>
 
-        <div className="absolute right-52 top-[16px] text-black text-xl font-bold font-pretendard">마이페이지</div>
+        {/* 마이페이지 링크 */}
+        <Link href="/mypage" className="absolute right-52 top-[16px] text-black text-xl font-bold font-pretendard">
+          마이페이지
+        </Link>
 
-        <div className="absolute right-0 top-0 w-[162px] h-[57px]">
+        {/* 지금 질문하기 링크 */}
+        <Link href="/qna/question" className="absolute right-0 top-0 w-[162px] h-[57px]">
           <div className="absolute w-[162px] h-[57px] bg-[#f46b02] rounded-xl" />
           <div className="absolute left-[26px] top-[16px] text-white text-xl font-medium font-pretendard">
             지금 질문하기
           </div>
-        </div>
+        </Link>
       </div>
     </div>
   );
