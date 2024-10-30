395377af563352219ee53066326c21f32fe2bb20
thwogh <daymos999@gmail.com>
2024-10-30 10:21:09 +0900

    feat: 질문게시판 UI 수정

--- src/components/board/tags/YeopjeonTag.tsx
+++ src/components/board/tags/YeopjeonTag.tsx
@@ -3,7 +3,7 @@ import ImageWrapper from "./ImageWrapper";
 
 function YeopjeonTag({ point }: { point: number }) {
   return (
-    <span className="inline-block bg-black text-white px-3 py-1 mr-1 rounded-[15px] text-[12px] font-pretendard-semibold">
+    <span className="inline-block bg-black text-white px-2 py-1 mr-[2px] rounded-[15px] text-[10px] font-pretendard-semibold">
       <ImageWrapper src="/icons/Yeopjeon.png" />
       <span className="ml-1">{point}</span>
     </span>
