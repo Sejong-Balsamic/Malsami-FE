716f6fda21005c17ed78227e9c035fc4ede535ba
NEWJIN <winniepooh00@naver.com>
2024-11-06 02:20:41 +0900

    feat: fetch Hot Question API 구현

--- src/components/landing/HotDocument.tsx
+++ src/components/landing/HotDocument.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import MovingCard from "../common/MovingCard";
+import MovingCard from "./MovingCardQuestion";
 
 function HotDocument() {
   return (
