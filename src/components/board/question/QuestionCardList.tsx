f97b64242eb68b21daf545e8a5010e45a45c8af8
thwogh <daymos999@gmail.com>
2024-11-09 00:05:40 +0900

    feat: 게시판 지정태그 영어->한국어로 번역

--- src/components/board/question/QuestionCardList.tsx
+++ src/components/board/question/QuestionCardList.tsx
@@ -32,21 +32,6 @@ interface QnaCardListProps {
 }
 
 export default function QuestionCardList({ categoryQNAs }: QnaCardListProps) {
-  // const [questions, setQuestions] = useState<Question[]>([]);
-
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       const data = await getCategoryQNAs();
-  //       setQuestions(data);
-  //     } catch (error) {
-  //       console.error("질문 목록을 가져오는 중 오류 발생:", error);
-  //     }
-  //   };
-
-  //   fetchData();
-  // }, []); // 빈 의존성 배열로 초기 렌더링 시 한 번만 실행
-
   return (
     <div>
       {categoryQNAs.map(question => (
