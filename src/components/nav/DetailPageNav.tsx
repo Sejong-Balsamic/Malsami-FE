52a5494584374ea2c9aeeefa9c4eb38b23dc1126
NEWJIN <winniepooh00@naver.com>
2024-11-12 23:02:48 +0900

    fix: build error

--- src/components/nav/DetailPageNav.tsx
+++ src/components/nav/DetailPageNav.tsx
@@ -7,7 +7,7 @@ function DetailPageNav() {
   return (
     <nav className="flex h-[80px] items-end justify-between px-5">
       {/* 뒤로 가기 아이콘 */}
-      <button type="button" className="flex mb-[34px]">
+      <button type="button" className="mb-[34px] flex">
         <Image src="/icons/BackIcon.svg" alt="썸네일" width={10} height={20} />
       </button>
 
@@ -16,7 +16,7 @@ function DetailPageNav() {
         {pathname === "/board/document/detail" ? (
           <>
             <h1 className="font-pretendard-bold text-xl">자료게시판</h1>
-            <div className="text-[#f46b01] font-pretendard-medium text-xs mt-1 mb-[10px]">양반등급</div>
+            <div className="font-pretendard-medium mb-[10px] mt-1 text-xs text-[#f46b01]">양반등급</div>
           </>
         ) : (
           <h1 className="font-pretendard-bold text-xl">질문게시판</h1>
@@ -24,7 +24,7 @@ function DetailPageNav() {
       </div>
 
       {/* 좌우 여백 맞추기 */}
-      <div className="w-2.5 h-2.5 bg-white rounded-full" />
+      <div className="h-2.5 w-2.5 rounded-full bg-white" />
     </nav>
   );
 }
