e14dc9dc2e69b9900bfe25451144c736badf4555
NEWJIN <winniepooh00@naver.com>
2024-11-13 15:49:12 +0900

    feat: doc detail nav option 구현

--- src/app/board/question/detail/page.tsx
+++ src/app/board/question/detail/page.tsx
@@ -4,7 +4,7 @@ import DetailPageNav from "@/components/nav/DetailPageNav";
 
 export default function Page() {
   return (
-    <div className="mx-auto w-full max-w-[640px]" style={{ height: "943px" }}>
+    <div className="mx-auto w-full max-w-[640px] bg-pink" style={{ height: "943px" }}>
       <DetailPageNav />
     </div>
   );
