0d830d79b827ac9b38e00b16e18432663787215c
NEWJIN <winniepooh00@naver.com>
2024-11-14 15:13:52 +0900

    feat: qna detail css

--- src/app/board/question/detail/page.tsx
+++ src/app/board/question/detail/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import DetailPageNav from "@/components/nav/DetailPageNav";
+import QnaDetail from "@/components/board/question/QnaDetail";
 
 export default function Page() {
   return (
     <div className="mx-auto w-full max-w-[640px] bg-pink" style={{ height: "943px" }}>
       <DetailPageNav />
+      <QnaDetail />
     </div>
   );
 }
