89d0d3606f86f80204eec08b86acce1eb76372e2
thwogh <daymos999@gmail.com>
2024-11-15 17:24:56 +0900

    feat(로그인): apiClient 에러케이스 추가

--- src/apis/clients/appClient.ts
+++ src/apis/clients/appClient.ts
@@ -64,6 +64,15 @@ apiClient.interceptors.response.use(
       }
     }
 
+    // 500 오류 처리
+    else if (error.response?.status === 500) {
+      console.error("서버 오류:", error.response.data || "알 수 없는 오류");
+      alert("서버에 문제가 발생했습니다. 잠시 후 다시 시도해주세요.");
+    } else {
+      console.error("알 수 없는 오류");
+      alert("서버에 문제가 발생했습니다. 잠시 후 다시 시도해주세요.");
+    }
+
     return Promise.reject(error); // 다른 에러는 그대로 전달
   },
 );
