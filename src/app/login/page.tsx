65474cafcb7c276f58796dfcd0f34bf66ef256ff
thwogh <daymos999@gmail.com>
2024-11-28 10:28:57 +0900

    feat(로그인페이지): 로그인 UI 개선

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,19 +1,3 @@
-// "use client";
-
-// import LoginForm from "@/components/login/LoginForm";
-// import ScrollToTopOnLoad from "@/components/common/ScrollToTopOnLoad";
-
-// export default function LoginPage() {
-//   return (
-//     <div className="flex min-h-screen items-center justify-center bg-gray-100">
-//       <ScrollToTopOnLoad />
-//       <div className="flex min-h-screen w-full min-w-[386px] max-w-[640px] flex-col items-center bg-white p-10">
-//         <h1 className="font-pretendard-bold mb-[4rem] text-center text-2xl">세종말싸미</h1>
-//         <LoginForm />
-//       </div>
-//     </div>
-//   );
-// }
 "use client";
 
 import LoginForm from "@/components/login/LoginForm";
