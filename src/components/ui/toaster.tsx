32762dbd6a08c2bf56c9edbdb93edb2b26edd81e
NEWJIN <winniepooh00@naver.com>
2024-12-01 21:48:16 +0900

    fix: setTimeout Toast Remove

--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -1,13 +1,22 @@
 /* eslint-disable */
 "use client";
 
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "@/store"; // Redux RootState 타입
 import { Toast, ToastDescription, ToastProvider, ToastTitle, ToastViewport } from "@/components/ui/toast";
+import { removeToast } from "@/store/toastSlice"; // removeToast 액션
 
 export function Toaster() {
   // Redux에서 toasts 상태 가져오기
+  const dispatch = useDispatch();
   const toasts = useSelector((state: RootState) => state.toast.toasts);
+
+  // 자동 제거 로직
+  toasts.forEach(({ id }) => {
+    setTimeout(() => {
+      dispatch(removeToast(id)); // Redux 상태에서 제거
+    }, 5000); // 5초 후 제거
+  });
 
   return (
     <ToastProvider>
