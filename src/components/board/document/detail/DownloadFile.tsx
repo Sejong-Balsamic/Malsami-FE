1b84be88d320501f4a60c51fa220a938383f71f4
NEWJIN <winniepooh00@naver.com>
2024-12-10 18:46:18 +0900

    feat: 자료 다운로드 버튼 출력값 수정

--- src/components/board/document/detail/DownloadFile.tsx
+++ src/components/board/document/detail/DownloadFile.tsx
@@ -1,16 +1,39 @@
 import Image from "next/image";
 
-function DownloadFile() {
+interface DocumentFile {
+  documentFileId: string;
+  filePath: string;
+  fileSize: number;
+  downloadCount: number;
+}
+
+interface DownloadFileProps {
+  documentFiles: DocumentFile[];
+}
+
+function formatFileSize(fileSize: number): string {
+  // 파일 크기를 MB로 변환
+  return (fileSize / (1024 * 1024)).toFixed(1); // MB 단위, 소수점 1자리 표시
+}
+
+function DownloadFile({ documentFiles }: DownloadFileProps) {
   return (
-    <div className="my-[30px] flex h-[58px] w-[270px] items-center justify-between rounded-[10px] bg-[#f7f8fb] p-[14px]">
-      <div className="flex flex-col">
-        <div className="font-pretendard-medium text-[12px]">첨부파일 이름.pdf</div>
-        <div className="font-pretendard-medium text-[12px] text-[#737373]">3.3MB</div>
-      </div>
-      <div className="flex flex-col items-center justify-center gap-1">
-        <Image src="/icons/Download.svg" alt="Download" width={12} height={15} />
-        <div className="font-pretendard-medium text-[12px] text-[#737373]">다운 25회</div>
-      </div>
+    <div className="my-[30px] flex flex-col gap-4">
+      {documentFiles.map(file => (
+        <div
+          key={file.documentFileId} // documentFileId를 key로 사용
+          className="flex h-[58px] w-[270px] items-center justify-between rounded-[10px] bg-[#f7f8fb] p-[14px]"
+        >
+          <div className="flex flex-col">
+            <div className="font-pretendard-medium text-[12px]">{file.filePath}</div>
+            <div className="font-pretendard-medium text-[12px] text-[#737373]">{formatFileSize(file.fileSize)}MB</div>
+          </div>
+          <div className="flex flex-col items-center justify-center gap-1">
+            <Image src="/icons/Download.svg" alt="Download" width={12} height={15} />
+            <div className="font-pretendard-medium text-[12px] text-[#737373]">다운 {file.downloadCount}회</div>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
