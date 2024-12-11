a18d040e396114f5c62c7081f80fc04764295704
NEWJIN <winniepooh00@naver.com>
2024-12-11 16:07:43 +0900

    fix: 로그인 유무를 반영한 리다이랙트 페이지 수정
    #294

--- src/components/landing/HotDocument.tsx
+++ src/components/landing/HotDocument.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
@@ -25,6 +24,15 @@ function HotDocument() {
     getWeeklyData();
     getDailyData();
   }, []);
+
+  const checkAccessTokenAndNavigate = (path: string) => {
+    const accessToken = sessionStorage.getItem("accessToken");
+    if (!accessToken) {
+      window.location.href = "/login";
+    } else {
+      window.location.href = path;
+    }
+  };
 
   return (
     <Tabs defaultValue="weekend" className="z-40 h-auto w-full">
@@ -52,11 +60,12 @@ function HotDocument() {
             <MovingCardDocument data={weekData} />
           </CardContent>
           <CardFooter>
-            <Link href="/board/document" passHref className="mx-3 w-full">
-              <Button className="font-pretendard-semibold h-[30px] w-full rounded-[10px] bg-[#03b8a3] text-[12px] text-white">
-                더보기
-              </Button>
-            </Link>
+            <Button
+              onClick={() => checkAccessTokenAndNavigate("/board/document/sub/popular-weekly")}
+              className="font-pretendard-semibold h-[30px] w-full rounded-[10px] bg-[#03b8a3] text-[12px] text-white"
+            >
+              더보기
+            </Button>
           </CardFooter>
         </Card>
       </TabsContent>
@@ -74,11 +83,12 @@ function HotDocument() {
             <MovingCardDocument data={dayData} />
           </CardContent>
           <CardFooter>
-            <Link href="/board/document" passHref className="mx-3 w-full">
-              <Button className="font-pretendard-semibold h-[30px] w-full rounded-[10px] bg-[#03b8a3] text-[12px] text-white">
-                더보기
-              </Button>
-            </Link>
+            <Button
+              onClick={() => checkAccessTokenAndNavigate("/board/document/sub/popular-daily")}
+              className="font-pretendard-semibold h-[30px] w-full rounded-[10px] bg-[#03b8a3] text-[12px] text-white"
+            >
+              더보기
+            </Button>
           </CardFooter>
         </Card>
       </TabsContent>
