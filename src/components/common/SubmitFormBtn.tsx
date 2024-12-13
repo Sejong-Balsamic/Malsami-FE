0c0001e7db3897deb7be0c67a0b453eaf4c7ca1c
thwogh <daymos999@gmail.com>
2024-12-13 15:06:37 +0900

    feat(글작성): 교과목명 잘못입력시 경고표시 UI개선

--- src/components/common/SubmitFormBtn.tsx
+++ src/components/common/SubmitFormBtn.tsx
@@ -1,9 +1,17 @@
-export default function SubmitFormBtn({ onClick }: { onClick: () => void }) {
+interface SubmitFormBtnProps {
+  onClick: () => void;
+  disabled?: boolean;
+}
+
+export default function SubmitFormBtn({ onClick, disabled = false }: SubmitFormBtnProps) {
   return (
     <button
       type="button"
-      className="font-pretendard-semibold h-10 w-full rounded-[10px] bg-custom-blue-400 py-[10px] text-base text-white hover:bg-custom-blue-500"
+      className={`font-pretendard-semibold h-10 w-full rounded-[10px] py-[10px] text-base text-white ${
+        disabled ? "cursor-not-allowed bg-gray-300" : "bg-custom-blue-400 hover:bg-custom-blue-500"
+      }`}
       onClick={onClick}
+      disabled={disabled} // 버튼 비활성화 처리
     >
       확인
     </button>
