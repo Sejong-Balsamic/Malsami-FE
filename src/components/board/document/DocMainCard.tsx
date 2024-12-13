1500ca985424bf6dc5b44ee900c396e432abf62f
thwogh <daymos999@gmail.com>
2024-12-13 13:01:05 +0900

    fix(자료메인): 카드 클릭 시 입장제한 후 페이지 이동 버그

--- src/components/board/document/DocMainCard.tsx
+++ src/components/board/document/DocMainCard.tsx
@@ -3,7 +3,7 @@ import { CardProps } from "@/types/DocCardProps.type";
 function DocMainCard({ color, subject, title }: CardProps) {
   return (
     <div className="flex items-center justify-center">
-      <div className="m-3 flex h-[114px] w-full flex-col">
+      <div className="m-3 flex h-[114px] w-[163px] flex-col">
         {/* 상단 파일 모양 */}
         <div className="flex h-[60px] items-end rounded-t-[16px] px-3.5 pb-2.5" style={{ backgroundColor: color }}>
           <span className="font-pretendard-semibold line-clamp-2 text-sm leading-tight text-white">{subject}</span>
