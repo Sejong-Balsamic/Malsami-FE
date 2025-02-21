bc4fb9742beee98fb7f0d76d79552f777eabd765
thwogh <daymos999@gmail.com>
2025-02-21 20:55:05 +0900

    CardList 구현 : feat : 랜딩페이지에 필요한 CardList구현 https://github.com/Sejong-Balsamic/Malsami-FE/issues/432

--- src/components/common/tags/CustomTag.tsx
+++ src/components/common/tags/CustomTag.tsx
@@ -6,7 +6,7 @@ type CustomTagProps = {
 
 function CustomTag({ tagName }: CustomTagProps) {
   return (
-    <span className="rounded-[14px] bg-[#DEFFD8] px-2.5 py-1 text-SUIT_12 font-semibold leading-[30px] text-[#00D342]">
+    <span className="rounded-[14px] bg-[#DEFFD8] px-2.5 py-[2px] text-SUIT_12 font-semibold leading-[20px] text-[#00D342]">
       {tagName}
     </span>
   );
--- src/components/common/tags/SubjectTag.tsx
+++ src/components/common/tags/SubjectTag.tsx
@@ -4,7 +4,7 @@ type SubjectTagProps = {
 
 function SubjectTag({ subject }: SubjectTagProps) {
   return (
-    <span className="line-clamp-1 rounded-sm border border-[#08E4BA] px-2.5 py-1 text-SUIT_12 font-semibold text-[#08E4BA]">
+    <span className="line-clamp-1 w-fit rounded-sm border border-[#08E4BA] px-2.5 py-1 text-SUIT_12 font-semibold text-[#08E4BA]">
       {subject}
     </span>
   );
