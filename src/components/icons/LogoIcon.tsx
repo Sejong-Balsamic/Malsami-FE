9a8e66b201fd7270178238be8569bf417dc9bff0
SUH SAECHAN <83532821+Cassiiopeia@users.noreply.github.com>
2025-06-18 18:11:37 +0900

    3D화 로고로 교체 필요 : feat : new 로고 교체 https://github.com/Sejong-Balsamic/Malsami-FE/issues/510

--- src/components/icons/LogoIcon.tsx
+++ src/components/icons/LogoIcon.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 // eslint-disable-next-line react/function-component-definition
 const LogoIcon: React.FC = () => {
-  return <img src="/image/logo.webp" alt="Logo" className="h-8 w-auto" />;
+  return <img src="/image/logo.webp" alt="Logo" width={40} height={40} />;
 };
 
 export default LogoIcon;
