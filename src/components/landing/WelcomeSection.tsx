405669cd2357ec66f1ff28c3b5a576062da05f1a
thwogh <daymos999@gmail.com>
2025-06-18 15:26:37 +0900

    검색바 구현 : feat : @눌러서 교과목 추천 z-index 수정 https://github.com/Sejong-Balsamic/Malsami-FE/issues/471

--- src/components/landing/WelcomeSection.tsx
+++ src/components/landing/WelcomeSection.tsx
@@ -11,7 +11,7 @@ interface WelcomeSectionProps {
 export default function WelcomeSection({ userName }: WelcomeSectionProps) {
   return (
     <div className="flex flex-col">
-      <div className="z-10 flex w-full flex-row items-center">
+      <div className="z-40 flex w-full flex-row items-center">
         <Image src="/image/Mascot.png" alt="웰컴 캐릭터" width={100} height={100} className="mr-3 object-contain" />
         <div className="flex w-full">
           <div className="text-SUIT_18 font-medium">
@@ -22,7 +22,7 @@ export default function WelcomeSection({ userName }: WelcomeSectionProps) {
       </div>
 
       {/* 검색바 - 위로 10px 겹치게 */}
-      <div className="z-0 -mt-[10px]">
+      <div className="-mt-[10px]">
         <LandingSearchBar />
       </div>
     </div>
