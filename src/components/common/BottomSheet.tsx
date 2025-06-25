b06d2500fbe48c687574a21a0dc67a508463cc8b
thwogh <daymos999@gmail.com>
2025-06-25 10:49:17 +0900

    필터링 구현 : feat : 필터링 하단 버튼 구현 https://github.com/Sejong-Balsamic/Malsami-FE/issues/435

--- src/components/common/BottomSheet.tsx
+++ src/components/common/BottomSheet.tsx
@@ -7,11 +7,11 @@ import {
   DrawerClose,
   DrawerTrigger,
 } from "@/components/shadcn/drawer";
-import { Button } from "@/components/shadcn/button";
 import { useDispatch, useSelector } from "react-redux";
 import { setIsOpen } from "@/global/store/bottomSheetSlice";
 import { RootState } from "@/global/store";
 import IconWrapper21x21 from "./IconWrapper21x21";
+import FilteringButton from "./buttons/FilteringButton";
 
 interface BottomSheetProps {
   onReset: () => void; // 초기화버튼 함수
@@ -39,7 +39,7 @@ export default function BottomSheet({ onReset, onConfirm, children: mainContent,
   // 확인 함수: 확인 후 닫기
   const handleConfirm = () => {
     onConfirm();
-    dispatch(setIsOpen(false)); // 확인 후 닫기
+    dispatch(setIsOpen(false));
   };
 
   return (
@@ -50,7 +50,7 @@ export default function BottomSheet({ onReset, onConfirm, children: mainContent,
       <DrawerContent className="mx-auto flex max-h-[80vh] w-full max-w-[640px] flex-col rounded-t-[30px] p-0">
         {/* 헤더: 필터링, X버튼 */}
         <DrawerHeader className="sticky top-0 flex flex-row items-center justify-between rounded-t-[30px] border-b-2 border-[#F3F3F3] px-[30px] pb-[26px] pt-[10px]">
-          <DrawerTitle className="text-SUIT_18 font-semibold">필터링</DrawerTitle>
+          <DrawerTitle className="font-suit-semibold text-[18px]">필터링</DrawerTitle>
           <DrawerClose onClick={handleClose}>
             <IconWrapper21x21 src="/icons/actions/x-lg.svg" />
           </DrawerClose>
@@ -60,14 +60,9 @@ export default function BottomSheet({ onReset, onConfirm, children: mainContent,
         <div className="overflow-y-auto px-[30px] pt-[26px]">{mainContent}</div>
 
         {/* 하단: 초기화 버튼, 확인 버튼 */}
-        {/* TODO: 피그마의 버튼으로 바꾸어야 함 */}
-        <div className="sticky bottom-0 flex justify-between p-6">
-          <Button variant="outline" className="rounded-full bg-gray-200 px-4 py-2 text-black" onClick={handleReset}>
-            초기화
-          </Button>
-          <Button className="rounded-full bg-green-500 px-4 py-2 text-white" onClick={handleConfirm}>
-            확인
-          </Button>
+        <div className="sticky bottom-0 flex gap-4 p-6">
+          <FilteringButton type="refresh" onClick={handleReset} />
+          <FilteringButton type="submit" onClick={handleConfirm} />
         </div>
       </DrawerContent>
     </Drawer>
