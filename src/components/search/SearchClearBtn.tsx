2740f82f716b374d36e137bb25888505613dab00
SUH SAECHAN <chan4760@gmail.com>
2026-06-17 14:32:37 +0900

    죽은 컴포넌트 청산 : refactor : import 어디에서도 참조되지 않는 미사용 컴포넌트 21개 제거 (단어경계+import경로 이중검증 후 안전 제거, 빌드 통과 확인)

--- src/components/search/SearchClearBtn.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function SearchClearBtn({ onClick }: { onClick: () => void }) {
-  return (
-    <button type="button" onClick={onClick} className="ml-2.5 flex-shrink-0">
-      <Image src="/icons/CloseIcon.svg" alt="삭제" width={18} height={18} />
-    </button>
-  );
-}
