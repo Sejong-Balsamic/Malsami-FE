abccd3e2078469f70f6ebf370757a10a297bd97c
SUH SAECHAN <chan4760@gmail.com>
2026-06-17 09:11:39 +0900

    죽은 Redux 슬라이스 제거 : refactor : 사용처 0건인 필터·faculty 슬라이스 5개 삭제 및 store 등록 정리 https://github.com/Sejong-Balsamic/Malsami-FE/issues/654

--- src/global/store/index.ts
+++ src/global/store/index.ts
@@ -1,10 +1,5 @@
 // src/global/store/index.ts
 import { configureStore } from "@reduxjs/toolkit";
-import docHotDownFilterOptions from "@/global/store/docHotDownFilterOptionsSlice";
-import docMyFacultyFilterOptions from "@/global/store/docMyFacultyFilterOptionsSlice";
-import docRequestFilterOptions from "@/global/store/docRequestFilterOptionsSlice";
-import filterOptions from "@/global/store/filterOptionsSlice";
-import facultyState from "@/global/store/facultySlice";
 import activeTab from "@/global/store/activeTabSlice";
 import toast from "@/global/store/toastSlice";
 import modal from "@/global/store/modalSlice";
@@ -14,14 +9,9 @@ import auth from "@/global/store/authSlice";
 
 export const store = configureStore({
   reducer: {
-    facultyState,
-    filterOptions,
     activeTab,
     toast,
     modal,
-    docHotDownFilterOptions,
-    docMyFacultyFilterOptions,
-    docRequestFilterOptions,
     fcm: fcmReducer,
     bottomSheet,
     auth,
